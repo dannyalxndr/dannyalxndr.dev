66de171b4c62d0c02a4cedb6fa733ef5d4763add
Danny Jimenez <dannyalxndr@gmail.com>
2014-11-07 11:12:56 -0600

    created an admin and login view

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -1,8 +1,21 @@
+<!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<meta charset="utf-8">
+	<title>Project Site</title>
+	<meta name="generator" content="Bootply" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<!--[if lt IE 9]>
+		<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	<link href="css/admin.css" rel="stylesheet">
+	<link href="css/login.css" rel="stylesheet">
 </head>
 <body>
+
+@yield('content')
 
 </body>
 </html>
